d363d3d45de212f8dabbb89216a8ef33c540c741
nicolas <thiersnicolas@gmail.com>
2018-04-25 15:59:21 +0200

    Laatste commit woensdag 25/04

--- WebContent/WEB-INF/JSP/index.jsp
+++ WebContent/WEB-INF/JSP/index.jsp
@@ -1,4 +1,5 @@
 <%@page contentType='text/html' pageEncoding='UTF-8' session='false'%>
+<%@taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core'%>
 <!doctype html>
 <html lang='nl'>
 <head>
@@ -8,8 +9,19 @@
 	<link rel='stylesheet' href='styles/default.css'>
 </head>
 <body>
+<c:import url='/WEB-INF/JSP/menu.jsp'/>
 	<h1>Pizza Luigi</h1>
 	<img src= 'images/pizza.jpg' alt='pizza' class='fullwidth'>
 	<h2>${begroeting}</h2>
+	<h2>De zaakvoerder</h2>
+	<dl>
+	<dt>Naam</dt><dd>${zaakvoerder.naam}</dd>
+	<dt>Aantal kinderen</dt><dd>${zaakvoerder.aantalKinderen}</dd>
+	<dt>Gehuwd</dt><dd>${zaakvoerder.gehuwd ? 'Ja' : 'Nee'}</dd>
+	<dt>Adres</dt><dd>${zaakvoerder.adres.straat}<br>
+	${zaakvoerder.adres.huisNr}<br>
+	${zaakvoerder.adres.postcode}<br>
+	${zaakvoerder.adres.gemeente}</dd>
+	</dl>
 </body>
 </html>
--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -1,13 +1,16 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import be.vdab.entities.Adres;
+import be.vdab.entities.Begroeting;
+import be.vdab.entities.Persoon;
 
 /**
  * Servlet implementation class IndexServlet
@@ -21,9 +24,8 @@ public class IndexServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request,HttpServletResponse response) 
 	throws ServletException, IOException {
-		int uur = LocalDateTime.now().getHour();
-		request.setAttribute("begroeting",
-				uur >= 6 && uur < 12 ? "Goede morgen" : uur >= 12 && uur < 18 ? "Goede middag" : "Goede avond");
+		request.setAttribute("begroeting",new Begroeting());
+		request.setAttribute("zaakvoerder", new Persoon("Luigi", "Peperone", 7, true, new Adres("Grote markt", "3", 9700, "Oudenaarde")));
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
